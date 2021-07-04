14c596b75e6018579ab8de75b9083002d1b2b4e4
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-07-04 10:46:23 +0500

    changed db string to online db

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-exports.connection = mongoose.connect("mongodb://localhost:27017/ShipMe", {
+exports.connection = mongoose.connect("mongodb+srv://muqeet_chaudhary:Abdul6890060@cluster0.bqu75.mongodb.net/ShipMe", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
